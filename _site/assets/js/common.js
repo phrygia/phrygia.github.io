ccd3487959609b7ace184b0ef5f1136761804f3a
chaeyeonLee <dmsgp62@gmail.com>
2020-12-27 15:26:52 +0900

    애널리틱스 코드 수정

--- _site/assets/js/common.js
+++ _site/assets/js/common.js
@@ -13,26 +13,16 @@ Top.addEventListener('click', function(){
 })
 
 
-// var Top = document.getElementById('ddd');
-
-// window.addEventListener('scroll', function() {
-//     if(this.scrollY > 200) {
-//         Top.classList.add('on');
-//     } else {
-//         Top.classList.remove('on');
-//     }
-// })
-
-// // Top.addEventListener('click', function(){
-// //     window.scrollTo({top: 0, behavior: 'smooth'});
-// // })
+// var Top2 = document.querySelector('button');
 
 
-// // var scrollPosition = $($(this).attr("data-target")).offset().top;
-// // const Top = document.getElementById('go-to-top');
-// Top.addEventListener('click', function(){
-//     let scrollPosition = (this.getAttribute("data-target")).offsetTop
-//     console.log(scrollPosition)
-//     console.log(this.getAttribute("data-target"))
-//     window.scrollTo({top: scrollPosition, behavior: 'smooth'});
-// })
+// var scrollPosition = $($(this).attr("data-target")).offset().top;
+// const Top = document.getElementById('go-to-top');
+
+document.querySelector('button').addEventListener('click', function(){
+    let scrollPosition = this.getAttribute("data-target")
+    console.log(scrollPosition)
+    console.log(scrollPosition.offsetTop)
+    // console.log(this.getAttribute("data-target"))
+    // window.scrollTo({top: scrollPosition, behavior: 'smooth'});
+})
--- assets/js/common.js
+++ assets/js/common.js
@@ -11,3 +11,18 @@ window.addEventListener('scroll', function() {
 Top.addEventListener('click', function(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+
+// var Top2 = document.querySelector('button');
+
+
+// var scrollPosition = $($(this).attr("data-target")).offset().top;
+// const Top = document.getElementById('go-to-top');
+
+document.querySelector('button').addEventListener('click', function(){
+    let scrollPosition = this.getAttribute("data-target")
+    console.log(scrollPosition)
+    console.log(scrollPosition.offsetTop)
+    // console.log(this.getAttribute("data-target"))
+    // window.scrollTo({top: scrollPosition, behavior: 'smooth'});
+})
