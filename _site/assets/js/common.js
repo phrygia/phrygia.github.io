3eb656e9ab78120b639f51f83a199a63dea6e75f
Lee Chaeyeon <dmsgp62@gmail.com>
2020-12-27 14:34:43 +0900

    포스팅 수정

--- _site/assets/js/common.js
+++ _site/assets/js/common.js
@@ -11,3 +11,28 @@ window.addEventListener('scroll', function() {
 Top.addEventListener('click', function(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+
+// var Top = document.getElementById('ddd');
+
+// window.addEventListener('scroll', function() {
+//     if(this.scrollY > 200) {
+//         Top.classList.add('on');
+//     } else {
+//         Top.classList.remove('on');
+//     }
+// })
+
+// // Top.addEventListener('click', function(){
+// //     window.scrollTo({top: 0, behavior: 'smooth'});
+// // })
+
+
+// // var scrollPosition = $($(this).attr("data-target")).offset().top;
+// // const Top = document.getElementById('go-to-top');
+// Top.addEventListener('click', function(){
+//     let scrollPosition = (this.getAttribute("data-target")).offsetTop
+//     console.log(scrollPosition)
+//     console.log(this.getAttribute("data-target"))
+//     window.scrollTo({top: scrollPosition, behavior: 'smooth'});
+// })
