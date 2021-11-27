84dd2f22de85b7a8dd84d83f95c7eba76153c7b8
chaeyeonLee <dmsgp62@gmail.com>
2021-11-27 20:02:45 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f59cf18f30208af341df.js"
+    "url": "webpack-runtime-dfe25e9d9c487a1ca91a.js"
   },
   {
-    "url": "styles.8ae5bf9bdcfc298317f2.css"
+    "url": "styles.56b16d625a5f6b501f56.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f142f5c464c720959d2ad5b03dd59598"
+    "revision": "3deef162871e047dca772831848d6714"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -63,7 +63,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b496a8c8d7953dadb8daae587dfea9a0"
+    "revision": "c54b16e2d7ca999ac57dbb0286983f14"
   },
   {
     "url": "polyfill-384ea82b9c97dbd7ee60.js"
