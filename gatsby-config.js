2c450575b1f881f0a0efd72ccb1e1006dde044c6
chaeyeonLee <dmsgp62@gmail.com>
2022-01-02 19:47:17 +0900

    PuTTY로 윈도우에서 aws ec2 ssh 접속하기 포스팅

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 720,
+              maxWidth: 1300,
               linkImagesToOriginal: false,
               backgroundColor: 'transparent',
             },
