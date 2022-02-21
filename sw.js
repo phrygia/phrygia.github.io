00876e48839b6122a2e56860c672dc2362bd0fa3
chaeyeonLee <dmsgp62@gmail.com>
2022-02-21 15:23:16 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9681f64560e4fb19685d.js"
+    "url": "webpack-runtime-65f8f0d5cf2e4e0b9cf5.js"
   },
   {
     "url": "styles.c246a64f895e2e42675c.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "965238cb348d92c0cdea0ed4ed09a0d0"
+    "revision": "76455fea1fa808948987cabb6eee568b"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
